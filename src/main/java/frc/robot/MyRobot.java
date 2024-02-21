b7a16913d21569610c32a0a414606056b8889258
thebayrobotics <frc8840@gmail.com>
2024-02-20 18:58:51 -0800

    changing CAN ids

--- src/main/java/frc/robot/MyRobot.java
+++ src/main/java/frc/robot/MyRobot.java
@@ -1,6 +1,8 @@
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.team_8840_lib.controllers.SwerveModule;
 import frc.team_8840_lib.info.console.Logger;
 import frc.team_8840_lib.listeners.EventListener;
 import frc.team_8840_lib.utils.async.Promise;
@@ -61,20 +63,28 @@ public class MyRobot extends EventListener {
     public void robotInit() {
         Logger.Log("Hello world!");
         robotContainer = new RobotContainer();
-        // frc.team_8840_lib.listeners.Robot.getRealInstance().waitForFullfillConditions(
-        // 3000,
-        // new Promise((res, rej) -> {
-        // Promise.WaitThen(() -> {
-        // return robotContainer.getSwerve().getSwerveDrive().isReady();
-        // }, res, rej, 10);
-        // }));
+        frc.team_8840_lib.listeners.Robot.getRealInstance().waitForFullfillConditions(
+        3000,
+        new Promise((res, rej) -> {
+        Promise.WaitThen(() -> {
+        return robotContainer.getSwerve().getSwerveDrive().isReady();
+        }, res, rej, 10);
+        }));
 
     }
 
     @Override
     public void robotPeriodic() {
-        // TODO Auto-generated method stub
         CommandScheduler.getInstance().run();
+        //Printing CANcoder angles
+        if (robotContainer!=null) {
+        SwerveModule[] modules = robotContainer.getSwerve().getSwerveDrive().getModules();
+        Logger.Log("Angles:");
+        Logger.Log("front left: " + modules[2].getAbsoluteAngle());
+        Logger.Log("front right: " + modules[0].getAbsoluteAngle());
+        Logger.Log("back right: " + modules[1].getAbsoluteAngle());
+        Logger.Log("back left: " + modules[3].getAbsoluteAngle());
+        }
     }
 
 }
