29b68e78344819857b06cc2b8bdc15b1abd02418
Teg Grenager <grenager@users.noreply.github.com>
2024-03-01 16:58:29 -0800

    unwrapping Arm encoders

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -7,7 +7,6 @@ import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants;
 import frc.robot.Settings;
 import frc.team_8840_lib.info.console.Logger;
 
